e91c37672592d0d9c7681128f97dd0b326cf068d
shicrystal <shinycrystal4553@gmail.com>
2022-06-20 14:14:45 +0900

    2nd

--- okrouter/src/main.jsx
+++ okrouter/src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/Contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
--- okrouter/src/pages/contact.jsx
+++ okrouter/src/pages/contact.jsx
@@ -1,9 +1,19 @@
 function Contact() {
+  const list = [1, 2, 3];
   return (
     <div>
       <h1>Contact</h1>
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>
+            <a href="">
+              {item}: {index}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
